358b903e857689ed2f87ba9e0f07c855430e801b
Antony Pavlenko <antonpawlenko@yandex.ru>
2024-09-16 20:18:06 +0300

    added all in INFO

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import logging
 import sys
 
 from aiogram import Bot, Dispatcher, html, types
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+
+from consts import start_msg, text1, text2, text3, text4, text5, text6, text7, text3_2
 
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = "7357167773:AAFRhw7Zr4FMBATfUaHNd96QmXxFrNOuIzI"
@@ -30,6 +34,202 @@ def make_kb_start():
     )
 
     return keyboard
+
+def make_inline_kb_start():
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="Программа", callback_data="1"
+    )
+    builder.button(
+        text="Расписание", callback_data="2"
+    )
+    builder.button(
+        text="Студенческие Организации", callback_data="3"
+    )
+    builder.button(
+        text="Студенческий Совет", callback_data="4"
+    )
+    builder.button(
+        text="Научные центры", callback_data="5"
+    )
+    builder.button(
+        text="Экскурсии в компании", callback_data="6"
+    )
+    builder.button(
+        text="Наука и искусство", callback_data="7"
+    )
+    # Выравниваем кнопки по 1 в ряд, чтобы получилось 4 + 1
+    builder.adjust(1)
+    return builder.as_markup()
+
+def make_inline_kb_for_kb1():
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="Расписание", callback_data="2"
+    )
+    builder.button(
+        text="Студенческие Организации", callback_data="3"
+    )
+    builder.button(
+        text="Студенческий Совет", callback_data="4"
+    )
+    builder.button(
+        text="Научные центры", callback_data="5"
+    )
+    builder.button(
+        text="Экскурсии в компании", callback_data="6"
+    )
+    builder.button(
+        text="Наука и искусство", callback_data="7"
+    )
+    # Выравниваем кнопки по 1 в ряд, чтобы получилось 4 + 1
+    builder.adjust(1)
+    return builder.as_markup()
+
+def make_inline_kb_for_kb2():
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="Программа", callback_data="1"
+    )
+    builder.button(
+        text="Студенческие Организации", callback_data="3"
+    )
+    builder.button(
+        text="Студенческий Совет", callback_data="4"
+    )
+    builder.button(
+        text="Научные центры", callback_data="5"
+    )
+    builder.button(
+        text="Экскурсии в компании", callback_data="6"
+    )
+    builder.button(
+        text="Наука и искусство", callback_data="7"
+    )
+    # Выравниваем кнопки по 1 в ряд, чтобы получилось 4 + 1
+    builder.adjust(1)
+    return builder.as_markup()
+
+def make_inline_kb_for_kb3():
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="Программа", callback_data="1"
+    )
+    builder.button(
+        text="Расписание", callback_data="2"
+    )
+    builder.button(
+        text="Студенческий Совет", callback_data="4"
+    )
+    builder.button(
+        text="Научные центры", callback_data="5"
+    )
+    builder.button(
+        text="Экскурсии в компании", callback_data="6"
+    )
+    builder.button(
+        text="Наука и искусство", callback_data="7"
+    )
+    # Выравниваем кнопки по 1 в ряд, чтобы получилось 4 + 1
+    builder.adjust(1)
+    return builder.as_markup()
+
+def make_inline_kb_for_kb4():
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="Программа", callback_data="1"
+    )
+    builder.button(
+        text="Расписание", callback_data="2"
+    )
+    builder.button(
+        text="Студенческие Организации", callback_data="3"
+    )
+    builder.button(
+        text="Научные центры", callback_data="5"
+    )
+    builder.button(
+        text="Экскурсии в компании", callback_data="6"
+    )
+    builder.button(
+        text="Наука и искусство", callback_data="7"
+    )
+    # Выравниваем кнопки по 1 в ряд, чтобы получилось 4 + 1
+    builder.adjust(1)
+    return builder.as_markup()
+
+def make_inline_kb_for_kb5():
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="Программа", callback_data="1"
+    )
+    builder.button(
+        text="Расписание", callback_data="2"
+    )
+    builder.button(
+        text="Студенческие Организации", callback_data="3"
+    )
+    builder.button(
+        text="Студенческий Совет", callback_data="4"
+    )
+    builder.button(
+        text="Экскурсии в компании", callback_data="6"
+    )
+    builder.button(
+        text="Наука и искусство", callback_data="7"
+    )
+    # Выравниваем кнопки по 1 в ряд, чтобы получилось 4 + 1
+    builder.adjust(1)
+    return builder.as_markup()
+
+def make_inline_kb_for_kb6():
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="Программа", callback_data="1"
+    )
+    builder.button(
+        text="Расписание", callback_data="2"
+    )
+    builder.button(
+        text="Студенческие Организации", callback_data="3"
+    )
+    builder.button(
+        text="Студенческий Совет", callback_data="4"
+    )
+    builder.button(
+        text="Научные центры", callback_data="5"
+    )
+    builder.button(
+        text="Наука и искусство", callback_data="7"
+    )
+    # Выравниваем кнопки по 1 в ряд, чтобы получилось 4 + 1
+    builder.adjust(1)
+    return builder.as_markup()
+
+def make_inline_kb_for_kb7():
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="Программа", callback_data="1"
+    )
+    builder.button(
+        text="Расписание", callback_data="2"
+    )
+    builder.button(
+        text="Студенческие Организации", callback_data="3"
+    )
+    builder.button(
+        text="Студенческий Совет", callback_data="4"
+    )
+    builder.button(
+        text="Научные центры", callback_data="5"
+    )
+    builder.button(
+        text="Экскурсии в компании", callback_data="6"
+    )
+    # Выравниваем кнопки по 1 в ряд, чтобы получилось 4 + 1
+    builder.adjust(1)
+    return builder.as_markup()
+
 
 class UserFestRega(StatesGroup):
     Name = State()
@@ -64,6 +264,60 @@ async def command_start_handler(message: Message, state: FSMContext) -> None:
 
         await message.answer(f"Hello user, {html.bold(message.from_user.full_name)}! введи ФИО", reply_markup=kb)
 
+        kkb = make_inline_kb_start()
+
+        await message.answer(start_msg, reply_markup=kkb)
+
+
+@dp.callback_query(F.data == "1")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.answer("ok")
+    await callback.bot.delete_message(callback.message.chat.id, callback.message.message_id)
+    kb = make_inline_kb_for_kb1()
+    await callback.bot.send_message(callback.message.chat.id, f"текст {text1}", reply_markup=kb)
+
+@dp.callback_query(F.data == "2")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.answer("ok")
+    await callback.bot.delete_message(callback.message.chat.id, callback.message.message_id)
+    kb = make_inline_kb_for_kb2()
+    await callback.bot.send_message(callback.message.chat.id, f"текст {text2}", reply_markup=kb)
+
+@dp.callback_query(F.data == "3")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.answer("ok")
+    await callback.message.answer(f"текст {text3}")
+    await callback.bot.delete_message(callback.message.chat.id, callback.message.message_id)
+    kb = make_inline_kb_for_kb3()
+    await callback.bot.send_message(callback.message.chat.id, f"текст {text3_2}", reply_markup=kb)
+
+@dp.callback_query(F.data == "4")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.answer("ok")
+    await callback.bot.delete_message(callback.message.chat.id, callback.message.message_id)
+    kb = make_inline_kb_for_kb4()
+    await callback.bot.send_message(callback.message.chat.id, f"текст {text4}", reply_markup=kb)
+
+@dp.callback_query(F.data == "5")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.answer("ok")
+    await callback.bot.delete_message(callback.message.chat.id, callback.message.message_id)
+    kb = make_inline_kb_for_kb5()
+    await callback.bot.send_message(callback.message.chat.id, f"текст {text5}", reply_markup=kb)
+
+@dp.callback_query(F.data == "6")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.answer("ok")
+    await callback.bot.delete_message(callback.message.chat.id, callback.message.message_id)
+    kb = make_inline_kb_for_kb6()
+    await callback.bot.send_message(callback.message.chat.id, f"текст {text6}", reply_markup=kb)
+
+@dp.callback_query(F.data == "7")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.answer("ok")
+    await callback.bot.delete_message(callback.message.chat.id, callback.message.message_id)
+    kb = make_inline_kb_for_kb7()
+    await callback.bot.send_message(callback.message.chat.id, f"текст {text7}", reply_markup=kb)
 
 
 @router.message(UserFestRega.Name)
