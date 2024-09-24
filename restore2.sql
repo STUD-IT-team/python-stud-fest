583d329f36cd96234565e940c5f6722275adf704
pai0id <andrey.polyakov05@gmail.com>
2024-09-24 18:13:21 +0300

    restore2

--- restore2.sql
+++ restore2.sql
@@ -1,2 +1,78 @@
-insert into member (tg, name, group_name)
-VALUES
+insert into member (tg, name, group_name, chat_id)
+VALUES ('nyan_cyan','Алешечкина Марина Романовна', 'Группа',1195316741);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('gagegy', 'Вострикова Полина Алексеевна', 'Группа', 1103380074); 
+insert into member (tg, name, group_name, chat_id)
+VALUES ('mishashtick','Шляков Михаил Васильевич', 'Группа', 1989282999); 
+insert into member (tg, name, group_name, chat_id)
+VALUES ('dassssshaaaa', 'Смирнова Дарья Сергеевна', 'Группа', 5209492917); 
+insert into member (tg, name, group_name, chat_id)
+VALUES ('A_Fom1ch', 'Фомичев Артём Евгеньевич', 'Группа', 1585726618); 
+insert into member (tg, name, group_name, chat_id)
+VALUES ('mainiania','Андреев Глеб Владимирович', 'Группа',1018813123);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('irinaablovatckaya','Абловацкая Ирина Сергеевна', 'Группа', 6517534571); 
+insert into member (tg, name, group_name, chat_id)
+VALUES ('whoyOuu','Ковалёв Арсений Павлович', 'Группа', 833764176); 
+insert into member (tg, name, group_name, chat_id)
+VALUES ('mmyyaauuu4u','Ярмолатий Маргарита Сергеевна', 'Группа',981662879);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('Mashuk10', 'Цветкова Мария Александаровна', 'Группа',1640318095);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('liiiizzaaa', 'Мухина Елизавета Андреевна', 'Группа', 601752914); 
+insert into member (tg, name, group_name, chat_id)
+VALUES ('olsaal', 'Суховилин Арсений Сергеевич', 'Группа',5404926586);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('dustyLuvy','Салюкина Олеся Александровна', 'Группа', 1595508412); 
+insert into member (tg, name, group_name, chat_id)
+VALUES ('StepVladik', 'Иванчихин Анатолий Георгиевич', 'Группа',602950710);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('PuIs4r', 'Якушева Камила Зефэровна', 'Группа', 957400692); 
+insert into member (tg, name, group_name, chat_id)
+VALUES ('mdfvbh', 'Маргиев Артур', 'Группа',103994666);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('lRlHA','Ложников Алексей Евгеньевич', 'Группа',513341531);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('Nadyakharicheva','Попова Марина Алексеевна', 'Группа', 1832922459); 
+insert into member (tg, name, group_name, chat_id)
+VALUES ('qwofuh', 'Харичева Надежда Олеговна', 'Группа',5187342462);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('habarcz','Богуславский Андрей Владимирович', 'Группа', 721328637); 
+insert into member (tg, name, group_name, chat_id)
+VALUES ('Owesttipe','Санников Николай Александрович', 'Группа', 641453770); 
+insert into member (tg, name, group_name, chat_id)
+VALUES ('RDHDME1','Никонов Егор Григорьевич', 'Группа', 862143713); 
+insert into member (tg, name, group_name, chat_id)
+VALUES ('simon_pimenov','Мельников Андрей Олегович', 'Группа',912296798);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('vitaliaKa','Пименов Семен Андреевич', 'Группа',832855849);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('semgamiu', 'Каминская Виталия Сергеевна', 'Группа',5215677120);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('rouumick', 'Кузнецова мария Ильинична', 'Группа',1261153543);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('mitya_06', 'Потапова Анна Алексеевна', 'Группа', 1300104807); 
+insert into member (tg, name, group_name, chat_id)
+VALUES ('lerunoo','Ковальчук Дмитрий Андреевич', 'Группа',1383521036);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('HotsMarch','Гапеева Валерия Юрьевна', 'Группа',5296616174);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('xandie_solo','Алексейкин Тимофей Васильевич', 'Группа',746338481);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('KambalenaA', 'Осинский Александр Михайлович', 'Группа',1025208529);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('Diabolus', 'Камбалина Алёна Владимировна', 'Группа', 1845843667); 
+insert into member (tg, name, group_name, chat_id)
+VALUES ('Periaate', 'Муслин Илья Павлович', 'Группа', 825100954); 
+insert into member (tg, name, group_name, chat_id)
+VALUES ('ggfffhkddhh','Бурдина Ксения Алексеевна', 'Группа',761599467);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('Lillymega','Исаенкова Дарья Андреевна', 'Группа',1316141417);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('fotestes', 'Корабейников Матвей Максимович', 'Группа', 795774321); 
+insert into member (tg, name, group_name, chat_id)
+VALUES ('leorended','Хабибуллин Леонид Маратович', 'Группа',1355603507);
+insert into member (tg, name, group_name, chat_id)
+VALUES ('shulmariia', 'Шульгина Мария Сергеевна', 'Группа', 902562091); 
+insert into member (tg, name, group_name, chat_id)
+VALUES ('cosnika234', 'Мастерина Злата Николаевна', 'Группа', 1420153152);
